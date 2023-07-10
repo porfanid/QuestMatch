143b1a36c6640680acef29a393b0b4f974091003
Paul Orfanidis <cs04134@uoi.gr>
2023-07-10 12:24:02 +0300

    Updated the backend to include all the necessary dependencies

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -1,26 +1,13 @@
+const express = require("express");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.authenticateUser = functions.https.onCall(function(data, context) {
-  const email = data.email;
-  const password = data.password;
+const app = express();
 
-  return admin.auth().getUserByEmail(email)
-      .then(function(user) {
-        return admin.auth().signInWithEmailAndPassword(user.email, password)
-            .then(function() {
-              return {message: "Authentication successful!"};
-            }).catch(function(error) {
-              console.error("Authentication error:", error);
-              throw new functions.https.HttpsError("internal",
-                  "Authentication failed!");
-            });
-      })
-      .catch(function(error) {
-        console.error("Authentication error:", error);
-        throw new functions.https.HttpsError("internal",
-            "Authentication failed!");
-      });
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
 });
+
+exports.api = functions.https.onRequest(app);
 
