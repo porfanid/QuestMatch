8c1d704f0c3b87b160510e2e3af3cdb6baa649b1
Paul Orfanidis <cs04134@uoi.gr>
2023-07-13 12:44:32 +0300

    Added a simple router to the project
    Easily switch between components Using the router component

---
+++ FrontEnd/front-end/src/About.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import logo from "./logo.svg";
+import App from "./App";
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    My name is Paul.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import About from "./About";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Home/>} />
+          {/* Add more routes as needed */}
+      </Routes>
+      </Router>
   );
 }
+
 
 export default App;
