fe342fc94dcaf3a046b991c429162a2177138ef6
Paul Orfanidis <cs04134@uoi.gr>
2023-07-20 16:47:06 +0300

    Fixed some style errors in the documentation

--- edit-documentation/QuestMatch/src/pages/index.js
+++ edit-documentation/QuestMatch/src/pages/index.js
@@ -24,7 +24,7 @@ function HomepageHeader() {
       </div>
       
       <section>
-    <iframe src="https://discord.com/widget?id=1130512537738285227&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    <iframe src="https://discord.com/widget?id=1130512537738285227&theme=dark" width="350" height="310" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </section>
     </header>
   );
