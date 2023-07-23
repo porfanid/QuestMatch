594f89ad800c6356278ba8b3c28e2de8b2efb6c6
Paul Orfanidis <cs04134@uoi.gr>
2023-07-23 08:44:34 +0300

    Added the firebase production url to the documentation page

--- edit-documentation/QuestMatch/src/pages/index.js
+++ edit-documentation/QuestMatch/src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            QuestMatch Tutorial - 5min ⏱️
+            to="https://questmatch2023.web.app">
+            QuestMatch Production Page
           </Link>
           
         </div>
