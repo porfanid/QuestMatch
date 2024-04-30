eac5e38437db6aa2ac97700019bfcd9a81c19be6
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-04-30 21:15:46 +0300

    Updated spells to show info

--- FrontEnd/front-end/src/CharacterSheet/Spells.js
+++ FrontEnd/front-end/src/CharacterSheet/Spells.js
@@ -1,87 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Modal, Button } from 'react-bootstrap';
 
 const SpellsOptions = ({ selectedClass, selectedRace, selectedLevel, setSelectedSpells }) => {
     const [spells, setSpells] = useState([]);
     const [selectedSpellIndices, setSelectedSpellIndices] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedSpellInfo, setSelectedSpellInfo] = useState(null);
 
-    const [maxSpells, setMaxSpells]=useState(0)
-
-    console.log("Selected Level is "+selectedLevel);
+    const components={
+        "V":"Verbal",
+        "S":"Somatic",
+        "M": "material components"
+    }
 
     useEffect(() => {
-        // Fetch spells from the API based on selected class and race
         let url = 'https://www.dnd5eapi.co/api/spells';
 
         if (selectedClass && selectedRace) {
-            url = `https://www.dnd5eapi.co/api/spells?classes=${selectedClass.index}&races=${selectedRace.index}&level=${selectedLevel}`;
+            url = `https://www.dnd5eapi.co/api/classes/${selectedClass.index}/spells?classes=&races=${selectedRace.index}&level=${selectedLevel}`;
         } else if (selectedClass) {
-            url = `https://www.dnd5eapi.co/api/spells?classes=${selectedClass.index}`;
+            url = `https://www.dnd5eapi.co/api/classes/${selectedClass.index}/spells`;
         } else if (selectedRace) {
             url = `https://www.dnd5eapi.co/api/spells?races=${selectedRace.index}`;
         }
-
-        axios.get(`https://www.dnd5eapi.co/api/classes/${selectedClass.index}/levels/${selectedLevel}`).then(
-            (response)=>{
-                console.log(response.data);
-                if(response.data.spellcasting){
-                    let total = 0;
-                    for (const key in response.data.spellcasting) {
-                        if (Object.prototype.hasOwnProperty.call(response.data.spellcasting, key)) {
-                            total += response.data.spellcasting[key];
-                        }
-                    }
-                    setMaxSpells(total);
-                    console.log(total);
-                }
-            }
-        )
 
         axios.get(url)
             .then(response => setSpells(response.data.results))
             .catch(error => console.error(error));
     }, [selectedClass, selectedRace, selectedLevel]);
 
-    // Event handler for selecting a spell
     const handleSpellClick = (spell) => {
-        // Check if the spell is already in the selectedSpells array
         const spellIndex = selectedSpellIndices.indexOf(spell.index);
-
-        if (spellIndex !== -1) {
-            // If the spell is already selected, remove it from the array
-            setSelectedSpellIndices(prevSelectedSpellIndices => prevSelectedSpellIndices.filter(index => index !== spell.index));
+        if (spellIndex === -1) {
+            setSelectedSpellIndices([...selectedSpellIndices, spell.index]);
         } else {
-            if(selectedSpellIndices.length>=maxSpells){
-                alert("You can't choose more spells.")
-            }else {
-                // If the spell is not selected, add it to the array
-                setSelectedSpellIndices(prevSelectedSpellIndices => [...prevSelectedSpellIndices, spell.index]);
-            }
+            setSelectedSpellIndices(selectedSpellIndices.filter(index => index !== spell.index));
         }
-        setSelectedSpells(selectedSpellIndices);
     };
 
-    // Get the selected spell objects based on their indices
+    const handleInfoClick = (spell) => {
+        // Fetch spell details from the spell's URL
+        axios.get(`https://www.dnd5eapi.co${spell.url}`)
+            .then(response => {
+                setSelectedSpellInfo(response.data);
+                console.log(response.data);
+                setShowModal(true);
+            })
+            .catch(error => console.error(error));
+    };
+
     const selectedSpells = selectedSpellIndices.map(index => spells.find(spell => spell.index === index));
+
+    const renderFormattedDescription = (descriptions) => {
+        return (
+            <ul>
+                {descriptions.map((desc, index) => (
+                    <li key={index} dangerouslySetInnerHTML={{__html:formatDescription(desc)}}/>
+                ))}
+            </ul>
+        );
+    };
+
+    const formatDescription = (description) => {
+        // Replace ***emphasize*** with <strong>emphasize</strong>
+        return description.replace(/\*\*\*(.*?)\*\*\*/g, '<strong>$1</strong>');
+    };
 
     return (
         <div className="container mt-5">
             <h1>Spells</h1>
             <div className="row mt-4">
                 <div className="col-md-12">
-                    <h2>{maxSpells-selectedSpellIndices.length} left</h2>
                     <div className="row">
                         {spells.map(spell => (
                             <div
                                 key={spell.index}
-                                className={`col mb-3`}
-                                onClick={() => handleSpellClick(spell)}
+                                className="col mb-3"
                                 style={{ cursor: 'pointer' }}
                             >
-                                <div className={`card ${selectedSpellIndices.includes(spell.index) ? 'bg-primary text-white' : 'bg-dark text-white'}`}>
+                                <div
+                                    className={`card ${selectedSpellIndices.includes(spell.index) ? 'bg-primary text-white' : 'bg-dark text-white'}`}
+                                    onClick={() => handleSpellClick(spell)}
+                                >
                                     <div className="card-body">
                                         <h5 className="card-title">{spell.name}</h5>
-                                        {/* Add more spell details as needed */}
+                                        <i className={"fa fa-circle-info"} onClick={(e) => { e.stopPropagation(); handleInfoClick(spell); }}     />
                                     </div>
                                 </div>
                             </div>
@@ -101,6 +105,44 @@ const SpellsOptions = ({ selectedClass, selectedRace, selectedLevel, setSelected
                     </div>
                 )}
             </div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header className="bg-dark">
+                    <Modal.Title>
+                        <h3 className={"text-primary"}>{selectedSpellInfo && selectedSpellInfo.name}</h3><br/>
+                        {selectedSpellInfo && selectedSpellInfo.ritual && <span className={"text-warning"}>Ritual</span>}
+                        <span className={"text-info"}>Range: {selectedSpellInfo && selectedSpellInfo.range}</span>
+                    </Modal.Title>
+
+
+                    <div>
+                        Components
+                        <ul>
+                            {selectedSpellInfo&&
+                                selectedSpellInfo.components.map((component, index) => (
+                                    <li key={index}>{components[component]}
+                                        <p>
+                                            {(component==="M")&&selectedSpellInfo.material}
+                                        </p>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </Modal.Header>
+                <Modal.Body className="bg-dark">
+                    {selectedSpellInfo && (
+                        <div>
+                            {renderFormattedDescription(selectedSpellInfo.desc)}
+                            {/* Add more spell details as needed */}
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer className="bg-dark">
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
