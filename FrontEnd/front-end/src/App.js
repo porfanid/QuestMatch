bd62e7e4b1a4a617a2983e75d4666f5066096365
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-05-01 20:41:23 +0300

    Updated bootstrap version and added page for the equipment

--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -20,7 +20,7 @@ function App() {
     const [selectedLevel, setSelectedLevel] = useState(null);
     const [ setSelectedSpells] = useState(null);
 
-    const test = (isLoggedIn) => {
+    const test = () => {
         if (isLoggedIn) {
             return (
                 <button onClick={logOut} className="btn btn-primary ml-auto">
@@ -68,63 +68,61 @@ function App() {
         setIsLoggedIn(localStorage.getItem('isLoggedIn') === 'true');
     }, [navigate]);
 
-    /**
-     *
-     * <Route path="about" element={<About/>} >
-     *               <Route path="" element={<About1/>} />
-     *               <Route path="about" element={<About2/>} />
-     *               <Route path="contact" element={<About2/>} />
-     *           </Route>
-     */
-
 
     return (
 
-      <>
-          <header>
-              <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                  <Link className="navbar-brand navbar-light" to="/">QuestMatch</Link>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                      <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarNav">
-                      <ul className="navbar-nav">
-                          <li className="nav-item">
-                              <NavLink to="/" className="nav-link" end>Home</NavLink>
-                          </li>
-                          <li className="nav-item">
-                              <NavLink exact to="/contribute" className="nav-link">Contribute</NavLink>
-                          </li>
-                          <li className="nav-item">
-                              <NavLink exact to="/character" className="nav-link">Create Character</NavLink>
-                          </li>
-                      </ul>
-                  </div>
-                  {
-                        test(isLoggedIn)
-                  }
-
-              </nav>
-          </header>
-
-
-      <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="contribute" element={<Contribute/>} />
-          <Route exact path="login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route exact path="signup" element={<Signup/>} />
-          <Route exact path="profile/" element={<Profile/>}/>
-          <Route exact path="character" element={<Character selectClass={setSelectedClass} selectRace={setSelectedRace} selectLevel={setSelectedLevel}/>}/>
-          <Route exact path="character/spells" element={<Spells setSelectedSpells={setSelectedSpells} selectedClass={selectedClass} selectedRace={selectedRace} selectedLevel={selectedLevel}/>}/>
-          <Route exact path="character/equipment" element={<Equipment/>}/>
-          <Route path="contact" element={<Home/>} />
-          {/* Add more routes as needed */}
-      </Routes>
+        <>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand text-light" to="/">QuestMatch</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarNav"
+                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <NavLink exact to="/" className="nav-link text-light"
+                                             activeClassName="active">Home</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact to="/contribute" className="nav-link text-light"
+                                             activeClassName="active">Contribute</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact to="/character" className="nav-link text-light"
+                                             activeClassName="active">Create Character</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        {test()}
+                    </div>
+                </nav>
+            </header>
 
 
-      </>
-  );
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="contribute" element={<Contribute/>}/>
+                <Route exact path="login" element={<Login setIsLoggedIn={setIsLoggedIn}/>}/>
+                <Route exact path="signup" element={<Signup/>}/>
+                <Route exact path="profile/" element={<Profile/>}/>
+                <Route exact path="character"
+                       element={<Character selectClass={setSelectedClass} selectRace={setSelectedRace}
+                                           selectLevel={setSelectedLevel}/>}/>
+                <Route exact path="character/spells"
+                       element={<Spells setSelectedSpells={setSelectedSpells} selectedClass={selectedClass}
+                                        selectedRace={selectedRace} selectedLevel={selectedLevel}/>}/>
+                <Route exact path="character/equipment" element={<Equipment/>}/>
+                <Route path="contact" element={<Home/>}/>
+                {/* Add more routes as needed */}
+            </Routes>
+
+
+        </>
+    );
 }
 
 
--- FrontEnd/front-end/src/CharacterSheet/Equipment.js
+++ FrontEnd/front-end/src/CharacterSheet/Equipment.js
@@ -1,8 +1,188 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {Button, Modal} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
 export default function Equipment(){
+
+    const [equipmentPack, setEquipmentPack] = useState([]);
+    const [selectedEquipmentPackIndices, setselectedEquipmentPackIndices] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedEquipmentPackInfo, setselectedEquipmentPackInfo] = useState(null);
+
+    const [selectedEquipment, setSelectedEquipment] = useState([]);
+    const [showSelectedEquipmentModal, setShowSelectedEquipmentModal] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        let url = 'https://www.dnd5eapi.co/api/equipment-categories/equipment-packs';
+
+        axios.get(url)
+            .then( async response => {
+                const results =await response.data.equipment;
+                setEquipmentPack(results);
+                console.log("Equipment Packs: ", results);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+
+    const handleEquipmentClick = (equipment) => {
+        const equipmentIndex = selectedEquipmentPackIndices.indexOf(equipment.index);
+        if (equipmentIndex === -1) {
+            setselectedEquipmentPackIndices([...selectedEquipmentPackIndices, equipment.index]);
+        } else {
+            setselectedEquipmentPackIndices(selectedEquipmentPackIndices.filter(index => index !== equipment.index));
+        }
+    };
+
+    const selectedEquipmentPack = selectedEquipmentPackIndices.map(index => equipmentPack.find(equipment => equipment.index === index));
+
+    const handleInfoClick = (equipment) => {
+        axios.get(`https://www.dnd5eapi.co${equipment.url}`)
+            .then(response => {
+                setselectedEquipmentPackInfo(response.data);
+                console.log(`https://www.dnd5eapi.co${equipment.url}`);
+                console.log(response.data);
+                setShowModal(true);
+            })
+            .catch(error => console.error(error));
+    };
+
+    const showDetailsAboutEquipment = (equipment) => {
+        console.log(equipment);
+        axios.get(`https://www.dnd5eapi.co${equipment.item.url}`)
+            .then(response => {
+                setSelectedEquipment(response.data);
+                console.log(response.data);
+                setShowSelectedEquipmentModal(true);
+            })
+            .catch(error => console.error(error));
+    }
+
+
     return(
-        <div>
-            <h1>Equipment</h1>
-            <p>Equipment will go here</p>
+        <div className="container mt-5">
+            <h1>Equipment Packs</h1>
+            <div className="row mt-4">
+                <div className="col-md-12">
+                    <div className="row">
+                        {equipmentPack&& equipmentPack.map(equipment => (
+                            <div
+                                key={equipment.index}
+                                className="col mb-3"
+                                style={{cursor: 'pointer'}}
+                            >
+                                <div
+                                    className={`card ${selectedEquipmentPackIndices.includes(equipment.index) ? 'bg-primary text-white' : 'bg-dark text-white'}`}
+                                    onClick={() => handleEquipmentClick(equipment)}
+                                >
+                                    <div className="card-body">
+                                        <h5 className="card-title">{equipment.name}</h5>
+                                        <i className={"fa fa-circle-info"} onClick={(e) => {
+                                            e.stopPropagation();
+                                            handleInfoClick(equipment);
+                                        }}/>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="mt-4">
+                {selectedEquipmentPack.length > 0 && (
+                    <div>
+                        <h4>Selected Equipment:</h4>
+                        <ul>
+                            {selectedEquipmentPack.map(equipment => (
+                                <li key={equipment.index}>{equipment.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <button onClick={() => {
+                    navigate("/character/equipment");
+                }} className="btn btn-primary btn-large">Submit
+                </button>
+            </div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header className="bg-dark" closeButton={true}>
+                    <Modal.Title>
+                        <h3 className={"text-primary"}>{selectedEquipmentPackInfo && selectedEquipmentPackInfo.name}</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="bg-dark">
+                    {selectedEquipmentPackInfo && (
+                        <div>
+                            {
+                                selectedEquipmentPackInfo.contents.map((equipment, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <h5>
+                                                <i className={"text-muted"}>Quantity:</i>
+                                                <i className={"text-info bg-gradient-primary"}>{equipment.quantity}</i>
+                                                <i className={"text-muted"}>Item:</i>
+                                                <i onClick={()=>{showDetailsAboutEquipment(equipment)}} className={"text-primary"}>{equipment.item.name}</i>
+                                            </h5>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer className="bg-dark">
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+
+
+
+
+
+            <Modal className={"bg-dark bg-opacity-50"} show={showSelectedEquipmentModal} onHide={() => setShowSelectedEquipmentModal(false)}>
+                <Modal.Header className="bg-dark" closeButton={true}>
+                    <Modal.Title>
+                        <h3 className={"text-primary"}>{selectedEquipment && selectedEquipment.name}</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="bg-dark">
+                    {selectedEquipment && (
+                        <div>
+                            {selectedEquipment.cost &&
+                            <div>
+                                Costs: {selectedEquipment.cost.quantity} {selectedEquipment.cost.unit}
+                            </div>
+                            }
+                            <div>
+                                Weight: {selectedEquipment.weight}
+                            </div>
+                            {selectedEquipment.gear_category &&
+                            <div>
+                                Gear Category: {selectedEquipment.gear_category.name}
+                            </div>
+                            }
+
+                            {selectedEquipment.desc[0] &&
+                                <div>
+                                    <span className={"text-info"}>Description:</span><br/> {selectedEquipment.desc[0]}
+                                </div>
+                            }
+
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer className="bg-dark">
+                    <Button variant="secondary" onClick={() => setShowSelectedEquipmentModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
